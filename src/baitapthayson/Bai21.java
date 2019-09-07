c0b06abc905d19167dc58199f81d450864ac5349
Lam <lamitptit99@gmail.com>
2019-09-08 00:28:39 +0700

    98

--- src/baitapthayson/Bai21.java
+++ src/baitapthayson/Bai21.java
@@ -5,18 +5,18 @@ import java.util.*;
 
 public class Bai21 {
     public static void main(String[] args) {
-        Set<Integer> set = new HashSet<>();
+        Set<Integer> set = new TreeSet<>();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] a =  new int[n];
         Random rd = new Random();
         for(int i = 0; i < n; i++){
             a[i]  = rd.nextInt(900)+100;
-            set.add(a[i]);
         }
         Arrays.sort(a);
         for(Integer b: a){
             System.out.print(b+" ");
+            set.add(b);
         }
         System.out.println();
         for(Integer b: set){
