632ef015dfc0689699343e5c9887bbf98d616c40
Lam <lamitptit99@gmail.com>
2019-11-05 21:32:00 +0700

    5/11

--- src/UI/UISV.java
+++ src/UI/UISV.java
@@ -14,62 +14,66 @@ import java.util.List;
 public class UISV extends JFrame {
     JPanel paneladd;
     static int maSVAuto = 1000;
-    List<SinhVien > listSV = new ArrayList<>();
-    JTextField ten,lop,dtb;
-    JButton addToFile,sort,readFromFile;
+    List<SinhVien> listSV = new ArrayList<>();
+    JTextField ten, lop, dtb;
+    JButton addToFile, sort, readFromFile;
     JTable table;
     DefaultTableModel model;
-    public UISV(){
+
+    public UISV() {
         init();
         setLayout(null);
         setBound();
-        docFile("SV.txt");
+        docFile("SVV.txt");
         addToFile.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String name,lopp,diem;
-                try{
+                String name, lopp, diem;
+                try {
                     name = ten.getText();
                     lopp = lop.getText();
                     diem = dtb.getText();
                     ExName(name);
                     ExName(lopp);
+                    ExLop(lopp);
                     ExDTB(Double.parseDouble(diem));
-                    SinhVien sv = new SinhVien(++maSVAuto,name,lopp,Double.parseDouble(diem));
+                    SinhVien sv = new SinhVien(++maSVAuto, name, lopp, Double.parseDouble(diem));
                     listSV.add(sv);
-                }catch(ExUsage ex){
+                    ghiFile("SVV.txt", sv);
+                } catch (ExUsage ex) {
                     ten.requestFocus();
-                    JOptionPane.showMessageDialog(null,"Nhap lai");
+                    JOptionPane.showMessageDialog(null, "Nhap lai");
                 }
-                ghiFile("SV.txt");
+
             }
         });
         sort.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Collections.sort(listSV,new SortByDTB());
+                Collections.sort(listSV, new SortByDTB());
                 int n = model.getRowCount();
-                for(int i = 0; i < n; i++)
+                for (int i = 0; i < n; i++)
                     model.removeRow(0);
-                for(SinhVien sv: listSV)
+                for (SinhVien sv : listSV)
                     hienthi(sv);
             }
         });
         readFromFile.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                docFile("SV.txt");
-                for(SinhVien sv: listSV)
+                docFile("SVV.txt");
+                for (SinhVien sv : listSV)
                     hienthi(sv);
             }
         });
-        setSize(1000,600);
+        setSize(1000, 600);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
     }
-    public void setBound(){
-        paneladd.setBounds(0,0,450,300);
+
+    public void setBound() {
+        paneladd.setBounds(0, 300, 450, 150);
         paneladd.add(new JLabel("Ten"));
         paneladd.add(ten);
         paneladd.add(addToFile);
@@ -83,94 +87,102 @@ public class UISV extends JFrame {
         add(paneladd);
 
     }
-    public void init(){
+
+    public void init() {
         ten = new JTextField();
         lop = new JTextField();
         dtb = new JTextField();
         addToFile = new JButton("Add to File");
         sort = new JButton("Sort");
         readFromFile = new JButton("Read from file");
-        paneladd = new JPanel(new GridLayout(3,3));
+        paneladd = new JPanel(new GridLayout(3, 3));
         model = new DefaultTableModel();
         table = new JTable(model);
         model.addColumn("ID");
         model.addColumn("Ten");
         model.addColumn("Lop");
         model.addColumn("DTB");
-        table.setBounds(500,100,400,600);
+        table.setBounds(500, 100, 400, 600);
         add(table);
     }
-    public void hienthi(SinhVien sv){
-        //for(SinhVien sv: listSV){
-            Object[] o = new Object[4];
-            o[0] = String.valueOf(sv.getMaSV());
-            o[1] = sv.getTen();
-            o[2] = sv.getLop();
-            o[3] = String.valueOf(sv.getDtb());
-            model.addRow(o);
-        //}
+
+    public void hienthi(SinhVien sv) {
+        Object[] o = new Object[4];
+        o[0] = String.valueOf(sv.getMaSV());
+        o[1] = sv.getTen();
+        o[2] = sv.getLop();
+        o[3] = String.valueOf(sv.getDtb());
+        model.addRow(o);
     }
-    public void docFile(String path){
+
+    public void docFile(String path) {
         listSV.clear();
         int n = model.getRowCount();
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
             model.removeRow(0);
-        FileInputStream fis = null;
         try {
-            fis = new FileInputStream(path);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        boolean cont = true;
-        try {
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            while(cont){
-                SinhVien sv = (SinhVien) ois.readObject();
-
-                if(sv!=null) {listSV.add(sv);maSVAuto = sv.getMaSV();}
-                else cont = false;
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String id, name, lop, dtb;
+            while ((id = br.readLine()) != null) {
+                name = br.readLine();
+                lop = br.readLine();
+                dtb = br.readLine();
+                SinhVien sv = new SinhVien(Integer.parseInt(id), name, lop, Double.parseDouble(dtb));
+                listSV.add(sv);
+                maSVAuto = Integer.parseInt(id);
             }
         } catch (Exception e) {
             //e.printStackTrace();
         }
     }
-    public void ghiFile(String path){
+
+    public void ghiFile(String path, SinhVien sv) {
         try {
-            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path));
-            for(SinhVien sv: listSV){
-                oos.writeObject(sv);
-            }
-            oos.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            FileWriter fw = new FileWriter(path, true);
+            fw.write(sv.getMaSV() + "\n");
+            fw.write(sv.getTen() + "\n");
+            fw.write(sv.getLop() + "\n");
+            fw.write(sv.getDtb() + "\n");
+            fw.close();
+        } catch (Exception e) {
         }
+
     }
+
     public static void main(String[] args) {
         new UISV();
     }
-    public void ExName(String s) throws ExUsage{
-        if(s.length()==0)   throw new ExUsage();
-    }
-    public void ExDTB(Double d) throws ExUsage{
-        if(d<0||d>10)   throw new ExUsage();
+
+    public void ExName(String s) throws ExUsage {
+        if (s.length() == 0) throw new ExUsage();
     }
 
+    public void ExDTB(Double d) throws ExUsage {
+        if (d < 0 || d > 10) throw new ExUsage();
+    }
+
+    public void ExLop(String s) throws ExUsage {
+        if (!s.matches("^D[1-9]{2}CQCN[0,1][0-9]$")) throw new ExUsage();
+    }
 }
-class SortByDTB implements Comparator<SinhVien>{
+
+class SortByDTB implements Comparator<SinhVien> {
 
     @Override
     public int compare(SinhVien o1, SinhVien o2) {
         return o1.getDtb() > o2.getDtb() ? -1 : 1;
     }
 }
-class ExUsage extends Exception{
-    public ExUsage(){
+
+class ExUsage extends Exception {
+    public ExUsage() {
 
     }
 }
-class SinhVien implements Serializable{
+
+class SinhVien implements Serializable {
     private int maSV;
-    private String ten,lop;
+    private String ten, lop;
     private double dtb;
 
     public SinhVien() {
@@ -197,5 +209,15 @@ class SinhVien implements Serializable{
 
     public double getDtb() {
         return dtb;
+    }
+
+    @Override
+    public String toString() {
+        return "SinhVien{" +
+                "maSV=" + maSV +
+                ", ten='" + ten + '\'' +
+                ", lop='" + lop + '\'' +
+                ", dtb=" + dtb +
+                '}';
     }
 }
--- src/UI/testJva.java
+++ src/UI/testJva.java
@@ -1,9 +1,42 @@
 package UI;
 
-public class testJva {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
+
+public class testJva  extends JFrame {
+    JTextField text;
+    JTextArea area;
+    JButton btn;
+    public testJva(){
+        init();
+        text.setBounds(0,0,100,50);
+        area.setBounds(100,200,200,100);
+        btn.setBounds(0,200,50,50);
+        add(text);
+        add(area);
+        add(btn);
+        setSize(600,400);
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                area.append(text.getText());
+            }
+        });
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setVisible(true);
+    }
+    public void init(){
+        area = new JTextArea();
+        area.setWrapStyleWord(true);
+        area.setLineWrap(true);
+        text = new JTextField();
+        btn = new JButton("Show");
+    }
     public static void main(String[] args) {
-        String s = "abc abc xyz";
-        s = s.replaceAll("abc","def");
-        System.out.println(s);
+        new testJva();
     }
 }
